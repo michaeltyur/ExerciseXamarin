88678c47197eaa1010ec9e5e4f2420bc0e57378c
michael <michaeltiour@gmail.com>
2018-10-25 10:00:29 +0300

    trttr

--- ExerciseXamarin/ExerciseXamarin/Controls/NavigationControl.xaml.cs
+++ ExerciseXamarin/ExerciseXamarin/Controls/NavigationControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using ExerciseXamarin.ViewModels;
+using Windows.UI.Xaml.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,6 +10,7 @@ namespace ExerciseXamarin.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NavigationControl : ContentView
 	{
+        #region Name Of Page
         public static readonly BindableProperty NameOfPageProperty = 
                                              BindableProperty.Create(nameof(NameOfPage),
                                                                      typeof(string), 
@@ -26,7 +28,9 @@ namespace ExerciseXamarin.Controls
                 SetValue(NameOfPageProperty, value);
             }
         }
+        #endregion
 
+        #region Home Button Text Value
         public static readonly BindableProperty ButtonTextValueProperty =
                                      BindableProperty.Create(nameof(HomeButtonTextValue),
                                                              typeof(string),
@@ -44,6 +48,67 @@ namespace ExerciseXamarin.Controls
                 SetValue(ButtonTextValueProperty, value);
             }
         }
+        #endregion
+
+        #region Home Text  Button
+        public static readonly BindableProperty TextButtonProperty =
+                                             BindableProperty.Create(nameof(HomeTextButton),
+                                                                     typeof(string),
+                                                                     typeof(NavigationControl),
+                                                                     default(string),
+                                                                     BindingMode.TwoWay);
+        public string HomeTextButton
+        {
+            get
+            {
+                return (string)GetValue(TextButtonProperty);
+            }
+            set
+            {
+                SetValue(TextButtonProperty, value);
+            }
+        }
+        #endregion
+
+        #region Home Button Background Color
+        public static readonly BindableProperty HomeButtonBackColorProperty =
+                                             BindableProperty.Create(nameof(HomeButtonBackColor),
+                                                                     typeof(Color),
+                                                                     typeof(NavigationControl),
+                                                                     default(Color),
+                                                                     BindingMode.TwoWay);
+        public Color HomeButtonBackColor
+        {
+            get
+            {
+                return (Color)GetValue(HomeButtonBackColorProperty);
+            }
+            set
+            {
+                SetValue(HomeButtonBackColorProperty, value);
+            }
+        }
+        #endregion
+
+        #region Home Button Text Color
+        public static readonly BindableProperty HomeButtonTextColorProperty =
+                                             BindableProperty.Create(nameof(HomeButtonTextColor),
+                                                                     typeof(Color),
+                                                                     typeof(NavigationControl),
+                                                                     default(Color),
+                                                                     BindingMode.TwoWay);
+        public Color HomeButtonTextColor
+        {
+            get
+            {
+                return (Color)GetValue(HomeButtonTextColorProperty);
+            }
+            set
+            {
+                SetValue(HomeButtonTextColorProperty, value);
+            }
+        }
+        #endregion
 
         public NavigationControl ()
 		{
@@ -56,7 +121,6 @@ namespace ExerciseXamarin.Controls
         }
 
       
-
         private void MainPageButton_Clicked(object sender, EventArgs e)
         {
             NameOfPage = Pages.MainPageView.ToString();
@@ -103,6 +167,37 @@ namespace ExerciseXamarin.Controls
             if (page == Pages.ItemsListPageView)
                 ListViewButton.IsEnabled = false;
             else ListViewButton.IsEnabled = true;
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            //home button
+            base.OnPropertyChanged(propertyName);
+            if (propertyName == TextButtonProperty.PropertyName)
+            {
+                MainPageButton.Text = HomeTextButton;
+            }
+            if (propertyName == HomeButtonBackColorProperty.PropertyName)
+            {
+                MainPageButton.BackgroundColor = HomeButtonBackColor;
+            }
+            if (propertyName == HomeButtonTextColorProperty.PropertyName)
+            {
+                MainPageButton.TextColor = HomeButtonTextColor;
+            }
+
+            //if (propertyName == SliderValueTextColorProperty.PropertyName)
+            //{
+            //    textColorSlider.Value = SliderValueTextColor;
+            //}
+            //if (propertyName == BackColorSliderLabelProperty.PropertyName)
+            //{
+            //    backColorSliderLabel.Text = BackColorSliderLabel;
+            //}
+            //if (propertyName == SliderValueBackColorProperty.PropertyName)
+            //{
+            //    backColorSlider.Value = SliderValueBackColor;
+            //}
         }
     }
 }
---
+++ ExerciseXamarin/ExerciseXamarin/Converters/DoubleToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace ExerciseXamarin.Converters
+{
+   public class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+
+            return new Color();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
